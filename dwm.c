180dbedc6567e7e4fc5dc884bffe342914285783
Delayless <Delayless@gmail.com>
2020-09-19 19:36:50 +0800

    modify some keybindings. patch focusmaster: mod+space switch focus to master.

--- dwm.c
+++ dwm.c
@@ -284,6 +284,8 @@ static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void xinitvisual();
 static void zoom(const Arg *arg);
 
+static void focusmaster(const Arg *arg);
+
 /* variables */
 static Systray *systray =  NULL;
 static const char broken[] = "broken";
@@ -2747,4 +2749,18 @@ main(int argc, char *argv[])
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
+}
+
+void
+focusmaster(const Arg *arg)
+{
+	Client *c;
+
+	if (selmon->nmaster < 1)
+		return;
+
+	c = nexttiled(selmon->clients);
+
+	if (c)
+		focus(c);
 }
