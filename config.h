d2cf51b45dc367d25f5b2b4423f9cddeca810085
Delayless <Delayless@gmail.com>
2020-09-23 09:58:30 +0800

    Revert "patch alpha." Use picom to implement transparency. Add `mark-ovredir-focused = false` to `picom.conf` for dwm.
    This reverts commit bf5e9982eae6ea8896339b61973b76249ea2b28e.

--- config.h
+++ config.h
@@ -23,18 +23,11 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#384B54";
 static const char col_border[]      = "#2F98ED";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_border},
 	[SchemeHid]  = { col_cyan,  col_gray1, col_border},
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
