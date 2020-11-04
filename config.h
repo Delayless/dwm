72dbbe8dd56a1c3c72431b7a2b70b7c454960f26
Delayless <Delayless@gmail.com>
2020-11-05 04:06:46 +0800

    patched dwm-movestack for moving windows.

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include "gaplessgrid.c"
+#include "movestack.c"
 
 #define PROJECT_PATH ~/Projects/Delayless
 #define STR1(PATH) #PATH
@@ -161,6 +162,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_r,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_r,      rotatestack,    {.i = -1 } },
 	{ MODKEY|ShiftMask,         XK_minus,      incnmaster,     {.i = +1 } },
