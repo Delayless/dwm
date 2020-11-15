4911a5f6069eb4dae43585863a8b1ea611cb506e
Delayless <Delayless@gmail.com>
2020-11-15 22:14:41 +0800

    Multi Monitor switch order.

--- config.h
+++ config.h
@@ -192,10 +192,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      tagtoleft,      {0} },
 	{ MODKEY|ShiftMask,             XK_l,      tagtoright,     {0} },
 	{ MODKEY,                       XK_x,      transfer,       {0} },
-	{ MODKEY,                   XK_comma,      focusmon,       {.i = -1 } },
-	{ MODKEY,                  XK_period,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,         XK_comma,      tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,        XK_period,      tagmon,         {.i = +1 } },
+	{ MODKEY,                  XK_period,      focusmon,       {.i = -1 } },
+	{ MODKEY,                   XK_comma,      focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,        XK_period,      tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,         XK_comma,      tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_e,      togglealttag,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
