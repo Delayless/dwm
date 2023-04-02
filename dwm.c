Delayless/dwm

commit e400388f4d87466549c63538eeaced5e7d2cbdc4

fix: close the feature that floating windows is always on top

dwm.c
@@ -2089,7 +2089,7 @@ restack(Monitor *m)
 		wc.stack_mode = Below;
 		wc.sibling = m->barwin;
 		for (c = m->stack; c; c = c->snext)
-			if (!c->isfloating && ISVISIBLE(c)) {
+			if (ISVISIBLE(c)) {
 				XConfigureWindow(dpy, c->win, CWSibling|CWStackMode, &wc);
 				wc.sibling = c->win;
 			}

config.h
@@ -229,7 +229,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[5]} },
-	/* { MODKEY|ControlMask,       XK_space,      setlayout,      {0} }, */
 	{ MODKEY|ShiftMask,         XK_space,      togglefloating, {0} },
 	{ MODKEY,                   XK_space,      focusmaster,    {0} },
 	{ MODKEY,                       XK_f,      togglefullscr,  {0} },

commit a53fb1534bf3b80f497a5750d5cd67593efbb4d6

fix/revert: display status bar when open a new tab 	    in a fullscreen chrome caused by the commit e400388f

dwm.c
@@ -611,7 +611,6 @@ attachstack(Client *c)
 void
 swallow(Client *p, Client *c)
 {
-
 	if (c->noswallow || c->isterminal)
 		return;
 	if (c->noswallow && !swallowfloating && c->isfloating)
@@ -1407,7 +1406,8 @@ focusstack(const Arg *arg)
 }
 
 void
-floatingwin(Monitor *m) {
+floatingwin(Monitor *m)
+{
 	Client * c;
 	// 程序里的arrange(NULL)好像不用变动
 	for (c=m->clients; c; c=c->next) {
@@ -2089,7 +2089,7 @@ restack(Monitor *m)
 		wc.stack_mode = Below;
 		wc.sibling = m->barwin;
 		for (c = m->stack; c; c = c->snext)
-			if (ISVISIBLE(c)) {
+			if (!c->isfloating && ISVISIBLE(c)) {
 				XConfigureWindow(dpy, c->win, CWSibling|CWStackMode, &wc);
 				wc.sibling = c->win;
 			}
@@ -2302,7 +2302,8 @@ setclientstate(Client *c, long state)
 }
 
 void
-tagtoleft(const Arg *arg) {
+tagtoleft(const Arg *arg)
+{
 	if(selmon->sel != NULL
 	&& __builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
 	&& selmon->tagset[selmon->seltags] > 1) {
@@ -2313,7 +2314,8 @@ tagtoleft(const Arg *arg) {
 }
 
 void
-tagtoright(const Arg *arg) {
+tagtoright(const Arg *arg)
+{
 	if(selmon->sel != NULL
 	&& __builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
 	&& selmon->tagset[selmon->seltags] & (TAGMASK >> 1)) {
@@ -3330,7 +3332,8 @@ view(const Arg *arg)
 }
 
 void
-viewtoleft(const Arg *arg) {
+viewtoleft(const Arg *arg)
+{
 	if(__builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
 	&& selmon->tagset[selmon->seltags] > 1) {
 		selmon->seltags ^= 1; /* toggle sel tagset */
@@ -3341,7 +3344,8 @@ viewtoleft(const Arg *arg) {
 }
 
 void
-viewtoright(const Arg *arg) {
+viewtoright(const Arg *arg)
+{
 	if(__builtin_popcount(selmon->tagset[selmon->seltags] & TAGMASK) == 1
 	&& selmon->tagset[selmon->seltags] & (TAGMASK >> 1)) {
 		selmon->seltags ^= 1; /* toggle sel tagset */